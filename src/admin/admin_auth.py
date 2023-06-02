31d7f0a4963e0ad2482662cea3a0a2abec5a6271
bashar <basharhasan74@gmail.com>
2023-06-02 23:05:08 +0300

    2 June

--- src/admin/admin_auth.py
+++ src/admin/admin_auth.py
@@ -1,19 +1,28 @@
 from typing import Optional
 from urllib.request import Request
 
-from fastapi import Depends
-from fastapi.security import HTTPBearer
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, OAuth2PasswordRequestForm
+from fastapi_users.authentication import backend
+from fastapi_users.authentication.strategy.db import strategy
+from redis import credentials
 from sqladmin.authentication import AuthenticationBackend
+from starlette import status
 from starlette.responses import RedirectResponse
 
+from auth.auth_manager import get_user_manager
 from auth.auth_models import User
 from auth.base_config import current_user
+from utils.error_code import ErrorCode
 
 
 class AdminAuth(AuthenticationBackend):
-    async def login(self, request: Request, dependencies=[Depends(HTTPBearer())],
-                    verified_user: User = Depends(current_user)) -> bool:
-        request.session.update({"token": "..."})
+    async def login(self, request: Request, user_manager =Depends(get_user_manager)) -> bool:
+        user_manager = get_user_manager()
+        user = await user_manager.authenticate(credentials)
+
+        response = await backend.login(strategy, user)
+        await user_manager.on_after_login(user, request, response)
         return True
 
     async def logout(self, request: Request) -> bool:
--- src/utils/global_depedency.py
+++
@@ -1,9 +0,0 @@
-from starlette.requests import Request
-from starlette.responses import Response
-
-
-class ResponseRequestDependency:
-    def __init__(self, request: Request, response: Response):
-        self.request = request
-        self.response = response
-
